dbc05495f6e40b7905851593131120201902f31c
Sushant Singh <sushantsingh@Sushants-MacBook-Air.local>
2025-08-05 13:31:17 +0530

    live url add in backend

--- backend/index.js
+++ backend/index.js
@@ -17,7 +17,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
 app.use(cors({
-    origin: "http://localhost:5173",
+    origin: "https://meetup-app-sushant-singh.onrender.com",
     credentials: true,
     methods: "GET,POST,PUT,DELETE",
     allowedHeaders: ["Content-Type", "Authorization"]
